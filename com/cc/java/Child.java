ea897bc0253e006ab7eaf0484cc2ba295d8b9370
BK <ck.comcave@proton.me>
2024-01-30 11:56:03 +0100

    Grandparent

--- com/cc/java/Child.java
+++ com/cc/java/Child.java
@@ -2,9 +2,9 @@ package com.cc.java;
 
 public class Child extends Parent{
 
-    public String sayHello(){
-        return "Hello from child!";
-    }
+    // public String sayHello(){
+    //     return "Hello from child!";
+    // }
 
 
 }
--- com/cc/java/Parent.java
+++ com/cc/java/Parent.java
@@ -1,10 +1,10 @@
 package com.cc.java;
 
-public class Parent {
+public class Parent extends GrandParent {
 
-    public String sayHello(){
-        return "Hello from parent!";
-    }
+    // public String sayHello(){
+    //     return "Hello from parent!";
+    // }
 
     public String sayHello(String str){
         return str;
